7b744cff18c3e2f22bad53831c289de58abe3493
qkrwogns9703 <jaehunb350@gmail.com>
2021-01-15 15:10:23 +0900

    Add local register

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -10,41 +10,30 @@ import {
   JoinTable,
   OneToMany,
 } from 'typeorm';
-
 import Drink from './Drinks';
 import Review from './Review';
 @Entity()
 export default class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
-
   @Column()
   userName: string;
-
   @Column()
   email: string;
-
-  @Column()
+  @Column({ default: 'socialPassword' })
   password: string;
-
   @Column({ default: 'noPath' })
   userImage: string;
-
   @Column({ default: 'local' })
   provider: string;
-
   @Column({ default: 0 })
   admin: number;
-
   @CreateDateColumn()
   createdAt: Date;
-
   @UpdateDateColumn()
   updatedAt: Date;
-
   @DeleteDateColumn()
   deletedAt: Date;
-
   @ManyToMany((type) => Drink)
   @JoinTable({
     name: 'drinks_bookmark',
@@ -58,7 +47,26 @@ export default class User extends BaseEntity {
     },
   })
   drinks: Drink[];
-
   @OneToMany((type) => Review, (review) => review.user)
   review: Review[];
+  static async localRegister(
+    email: string,
+    password: string,
+    userName: string
+  ): Promise<User | undefined> {
+    const { id } = (
+      await this.createQueryBuilder()
+        .insert()
+        .into(User)
+        .values([
+          {
+            email,
+            password,
+            userName,
+          },
+        ])
+        .execute()
+    ).identifiers[0];
+    return this.findOne({ id });
+  }
 }
