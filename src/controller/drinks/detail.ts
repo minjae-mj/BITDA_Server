1d19850831a235168e6986f22fd2edac485f2d4b
rameau17 <yoojin.rameau@gmail.com>
2021-01-15 15:16:44 +0900

    Add list complete

--- src/controller/drinks/detail.ts
+++ src/controller/drinks/detail.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from 'express';
-
+import Drink  from "../../entity/Drinks"
 export default async (req: Request, res: Response): Promise<void> => {
-	console.log('detail');
+	const detail = await Drink.detailView(2);
+	if (detail){
+		res.send(detail)
+	} else {
+		res.send({"message": "error message "})
+	}
 };
