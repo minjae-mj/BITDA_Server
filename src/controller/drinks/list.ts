f29660d3ebbeed90b91a71f4e462b250971e456c
rameau17 <yoojin.rameau@gmail.com>
2021-01-16 17:34:30 +0900

    Add drinks detail

--- src/controller/drinks/list.ts
+++ src/controller/drinks/list.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
-import Drink  from "../../entity/Drinks"
+import Drink from '../../entity/Drinks';
 
 export default async (req: Request, res: Response): Promise<void> => {
-	const drinkList = await Drink.allDrinkList();
-	
-	if (drinkList){
-		res.send(drinkList)
-	} else {
-		res.send({"message": "error message "})
-	}
+  try {
+    const drinkList = await Drink.allDrinkList();
+    if (drinkList) {
+      res.send(drinkList);
+    }
+  } catch (err) {
+    res.send({ message: 'error message ' });
+  }
 };
--- src/routes/drinks.ts
+++ src/routes/drinks.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import { drinkController } from '../controller';
-
+import checkToken from '../utils/sign/checkToken';
 const router = express.Router();
 
 router.get('/list', drinkController.list);
-router.get('/detail', drinkController.detail);
+router.get('/detail/:drinkId', checkToken, drinkController.detail);
 router.post('/list/type', drinkController.typeList);
 router.post('/like', drinkController.like);
 router.post('/unlike', drinkController.unlike);
